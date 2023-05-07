ee01a6f86ed3e5a653ff785cbb2287a157ef3ee3
Arian Hosseini <arianhosseini@outlook.com>
2023-05-07 16:23:26 +0330

    feat: created <Counter /> component to show the counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Counter from "./features/counter/Counter";
 import "./App.css";
 
 const App = () => {
-	return <div>Hello, World!</div>;
+	return (
+		<main className="App">
+			<Counter />
+		</main>
+	);
 };
 
 export default App;
